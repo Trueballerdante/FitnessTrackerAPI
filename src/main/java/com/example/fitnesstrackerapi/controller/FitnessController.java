bec4f1550e32dc980f7969aeb69920cb59fa5165
Dante Garcia <dag4799@yahoo.com>
2022-08-16 13:03:47 -0500

    created a simple test controller

--- src/main/java/com/example/fitnesstrackerapi/controller/FitnessController.java
+++ src/main/java/com/example/fitnesstrackerapi/controller/FitnessController.java
@@ -1,4 +1,20 @@
 package com.example.fitnesstrackerapi.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/v1")
 public interface FitnessController {
+
+    @GetMapping("/fitness")
+    String getFitnessInfo();
+
+    @PostMapping("/fitness")
+    void saveFitnessInfo();
+
+    @PutMapping("/fitness")
+    void updateFitnessInfo();
+
+    @DeleteMapping("/fitness")
+    void deleteFitnessInfo();
+
 }
--- src/main/java/com/example/fitnesstrackerapi/controller/impl/FitnessControllerImpl.java
+++ src/main/java/com/example/fitnesstrackerapi/controller/impl/FitnessControllerImpl.java
@@ -1,4 +1,25 @@
 package com.example.fitnesstrackerapi.controller.impl;
 
-public class FitnessControllerImpl {
+import com.example.fitnesstrackerapi.controller.FitnessController;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class FitnessControllerImpl implements FitnessController {
+
+    public String getFitnessInfo() {
+        return "Called getFitnessInfo";
+    }
+
+    public void saveFitnessInfo() {
+
+    }
+
+    public void updateFitnessInfo() {
+
+    }
+
+    public void deleteFitnessInfo() {
+
+    }
+
 }
